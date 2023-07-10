b538467d5f688b7e0eff51bd3e2a858ac15c1707
sakshamgoel-github <saksham.goel0808@gmail.com>
2023-07-10 15:29:37 +0530

    Minimum Depth of Binary Tree

---
+++ 111MinimumDepthofBinaryTree.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define turbo ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root)
+        return 0;
+
+        int left = 1e5, right = 1e5; 
+        if (root -> left)  
+        left = minDepth(root -> left);
+        if (root -> right)        
+        right = minDepth(root -> right);
+
+        if ((left == 1e5) && (right == 1e5)){
+            return 1;
+        }
+        return min(left,right) + 1;        
+    }
+};
+
+int main()
+{
+    turbo;
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+
+    // cerr<< "\ntime taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
