94a5000fbe7b683f48aca24b8e8e0a75b2637c5e
sakshamgoel-github <saksham.goel0808@gmail.com>
2023-07-12 15:14:20 +0530

    Longest Common Prefix

---
+++ 14LongestCommonPrefix.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define turbo ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& v) {
+        string ans="";
+        sort(v.begin(),v.end());
+        int n=v.size();
+        string first=v[0],last=v[n-1];
+        for(int i=0;i<min(first.size(),last.size());i++){
+            if(first[i]!=last[i]){
+                return ans;
+            }
+            ans+=first[i];
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    turbo;
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+
+    // cerr<< "\ntime taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
