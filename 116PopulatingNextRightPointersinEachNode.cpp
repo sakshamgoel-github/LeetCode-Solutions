2b217ed3d8b833744c4fd0d8ec89773ab7738e15
sakshamgoel-github <saksham.goel0808@gmail.com>
2023-09-20 17:10:34 +0530

    Populating Next Right Pointers in Each Node

---
+++ 116PopulatingNextRightPointersinEachNode.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define turbo                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node *next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node *_left, Node *_right, Node *_next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+class Solution
+{
+public:
+    Node *connect(Node *root)
+    {
+        if (root)
+        {
+            queue<Node *> q;
+            q.push(root);
+            while (!q.empty())
+            {
+                int n = q.size();
+                Node *curr = q.front();
+                q.pop();
+                if (curr->left)
+                    q.push(curr->left);
+                if (curr->right)
+                    q.push(curr->right);
+                for (int i = 0; i < n - 1; i++)
+                {
+                    curr->next = q.front();
+                    curr = q.front();
+                    q.pop();
+                    if(curr->left) q.push(curr->left);
+                    if(curr->right) q.push(curr->right);
+                }
+            }
+        }
+        return root;
+    }
+};
+
+int main()
+{
+    turbo;
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+
+    // cerr<< "\ntime taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
