39d7cca8c565008054166d6ff6babb650d4efe8c
sakshamgoel-github <saksham.goel0808@gmail.com>
2023-09-20 17:15:51 +0530

    700. Search in a Binary Search Tree

---
+++ 700SearchinaBinarySearchTree.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define turbo                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *searchBST(TreeNode *root, int val)
+    {
+        if (!root)
+            return NULL;
+        if (root->val == val)
+            return root;
+        if (root->val > val)
+            return searchBST(root->left,val);
+        return searchBST(root->right,val);
+    }
+};
+
+int main()
+{
+    turbo;
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+
+    // cerr<< "\ntime taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
