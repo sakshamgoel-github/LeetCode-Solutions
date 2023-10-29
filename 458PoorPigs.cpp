618fedf9a2145bfadc3512ada7a22d4deeb1caee
sakshamgoel-github <saksham.goel0808@gmail.com>
2023-10-29 16:52:53 +0530

    Poor Pigs

---
+++ 458PoorPigs.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define turbo ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+class Solution {
+public:
+    int poorPigs(int buckets, int timeDetect, int timeTest) {
+        return ceil(log2(buckets)/log2(int(timeTest/timeDetect)+1));
+    }
+};
+
+int main()
+{
+    turbo;
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+
+    // cerr<< "\ntime taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
