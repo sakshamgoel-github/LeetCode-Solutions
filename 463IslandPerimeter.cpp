217c66197a62a8064da8b5c370a4456004bd4165
sakshamgoel-github <saksham.goel0808@gmail.com>
2024-04-18 13:07:37 +0530

    Island Perimeter

---
+++ 463IslandPerimeter.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define turbo                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+class Solution
+{
+public:
+    int islandPerimeter(vector<vector<int>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        int perimeter = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    if (j - 1 < 0 || grid[i][j - 1] == 0)
+                        ++perimeter;
+                    if (j + 1 >= m || grid[i][j + 1] == 0)
+                        ++perimeter;
+                    if (i - 1 < 0 || grid[i - 1][j] == 0)
+                        ++perimeter;
+                    if (i + 1 >= n || grid[i + 1][j] == 0)
+                        ++perimeter;
+                }
+            }
+        }
+        return perimeter;
+    }
+};
+
+int main()
+{
+    turbo;
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+
+    // cerr<< "\ntime taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
+    return 0;
+}
